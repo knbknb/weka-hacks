b3b9a54bb50dba4ca374dbd9a0d29fa6284ea689
Knut Behrends <knb@gfz-potsdam.de>
2015-04-20 12:56:13 +0200

    include changes from wb3

--- weka-dev.sh
+++ weka-dev.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-WEKA_HOME=/opt/smallapps/weka-stable/weka
+WEKA_HOME=/opt/smallapps/weka-dev/weka
 export WEKA_JAR=$WEKA_HOME/weka.jar
 export PATH=$WEKA_HOME:$PATH
 CP="/usr/share/java/:/opt/dbvis9/jdbc/jtds/jtds.jar:/opt/database/sqlite/sqlite-jdbc.jar:$WEKA_JAR"
 echo "used CLASSPATH: $CP"
-DIR=/opt/smallapps/weka-stable/data/
+DIR=/opt/smallapps/weka-dev/data/
 echo "changing to '$DIR'"
 cd "$DIR"
 export WEKA_HOME=$DIR
